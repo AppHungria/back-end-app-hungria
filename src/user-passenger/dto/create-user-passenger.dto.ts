437f01c84a201e2d525c693182e7826ec41eda35
JogadorHungria <joilsonhungrianv@gmail.com>
2024-10-17 22:25:52 -0300

    feat: implement classe-validator / dto create user-passenger

--- src/user-passenger/dto/create-user-passenger.dto.ts
+++ src/user-passenger/dto/create-user-passenger.dto.ts
@@ -1,1 +1,36 @@
-export class CreateUserPassengerDto {}
+import { IsBoolean, IsEmail, IsNumber, IsOptional, IsString, MinLength } from "class-validator";
+
+export class CreateUserPassengerDto {
+
+    @IsString()
+    UserId: string
+
+    @IsString()
+    Name: string;
+
+    @IsEmail()
+    EmailAdress: string;
+
+    @IsString()
+    Password: string;
+
+    @IsNumber()
+    PhoneNumber: number;
+  
+    @IsString()
+    StatusAccount: "Aproved" | "Rejected" | "Analizing" ;
+  
+    @IsNumber()
+    RacesCanceleds : number;
+
+    @IsNumber()
+    RacesCompleteds : number;
+
+    @IsString()
+    @MinLength(6, { message: 'Password must be at least 6 characters long.' })
+    password: string;
+  
+    @IsString()
+    LastConnectedDeivice: string;
+
+}
--- src/user-passenger/entities/user-passenger.entity.ts
+++ src/user-passenger/entities/user-passenger.entity.ts
@@ -1,1 +1,24 @@
-export class UserPassenger {}
+export class UserPassenger {
+    
+    UserId: string
+    
+    Name: string;
+
+    EmailAdress: string;
+    
+    Password: string;
+  
+    PhoneNumber: number;
+  
+    StatusAccount: "Aproved" | "Rejected" | "Analizing" ;
+ 
+    RacesCanceleds : number;
+
+    RacesCompleteds : number;
+
+    password: string;
+
+    LastConnectedDeivice: string;
+
+}
+
